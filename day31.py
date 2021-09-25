9610b2403a09fab601ce00ca963fe59a3f4883d6
Renato Lewkowicz <renato.lew@gmail.com>
2021-09-24 21:33:42 -0300

    Day 31 exercise.

---
+++ day31.py
@@ -0,0 +1,32 @@
+# Write a Python program to check if a given value is present in a set or not.
+
+nums = {1, 2, 5, 6, 7, 9, 11, 14, 18}
+
+print("\nOriginal sets(nums): ",nums,"\n")
+
+print("\nTesting if 6 exists in nums:")
+print(6 in nums)
+
+print("\nTesting if 7 exists in nums:")
+print(7 in nums)
+
+print("\nTesting if 11 exists in nums:")
+print(11 in nums)
+
+print("\nTesting if 0 exists in nums:")
+print(0 in nums)
+
+print("\nTesting if 6 is not in nums")
+print(6 not in nums)
+
+print("\nTesting if 7 is not in nums")
+print(7 not in nums)
+
+print("\nTesting if 11 is not in nums")
+print(11 not in nums)
+
+print("\nTesting if 0 is not in nums")
+print(0 not in nums)
+
+print("\nTesting if 17 is not in nums")
+print(17 not in nums)
