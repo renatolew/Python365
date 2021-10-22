458f7993ff26bad59dac40afffcffa08b8d5138a
Renato Lewkowicz <renato.lew@gmail.com>
2021-10-21 23:07:19 -0300

    Day 55 exercise.

---
+++ day55.py
@@ -0,0 +1,10 @@
+# Write a Python program to calculate the sum of all items of a container (tuple, list, set, dictionary).
+
+import os
+
+file_size = os.path.getsize("day1.py")
+
+print("\nThe size of day1.py is :",file_size,"Bytes")
+
+print()
+
