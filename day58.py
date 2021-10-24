8ec4d2f1198557f329725cd3158d9ddd85ca407a
Renato Lewkowicz <renato.lew@gmail.com>
2021-10-24 19:01:44 -0300

    Day 58 exercise.

---
+++ day58.py
@@ -0,0 +1,12 @@
+# Write a Python program to calculate the hypotenuse of a right angled triangle.
+
+from math import sqrt
+
+print("Input lengths of shorter triangle sides:")
+
+a = float(input("a: "))
+b = float(input("b: "))
+
+c = sqrt(a**2 + b**2)
+
+print("The length of the hypotenuse is:", c )
