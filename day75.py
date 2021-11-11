ccfa8f0b46bba3e9b204bc99e44b2d3b7cce33b9
Renato Lewkowicz <renato.lew@gmail.com>
2021-11-10 22:38:23 -0300

    Day 75 exercise.

---
+++ day75.py
@@ -0,0 +1,6 @@
+# Write a NumPy program to find the number of weekdays in March 2017.
+
+import numpy as np
+
+print("Number of weekdays in March 2017:")
+print(np.busday_count('2017-03', '2017-04'))
