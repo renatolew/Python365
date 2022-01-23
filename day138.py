e480652fe6294264ac746ef10a95f7048438f8c0
Renato Lewkowicz <renato.lew@gmail.com>
2022-01-22 22:49:11 -0300

    Day 138 exercise.

---
+++ day138.py
@@ -0,0 +1,11 @@
+# Write a Pandas program to read rows 0 through 2 (inclusive), columns 'color' through 'price' (inclusive) of diamonds DataFrame.
+
+import pandas as pd
+
+diamonds = pd.read_csv('https://raw.githubusercontent.com/mwaskom/seaborn-data/master/diamonds.csv')
+
+print("Original Dataframe:")
+print(diamonds.head())
+
+print("\nRows 0 through 2 (inclusive), columns ‘color’ and ‘price’:")
+print(diamonds.loc[0:2, 'color':'price'])
