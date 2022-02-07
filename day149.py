9c9fd6035ee41a85ffbb12f533ce5d5492c6823b
Renato Lewkowicz <renato.lew@gmail.com>
2022-02-07 19:55:48 -0300

    Day 149 exercise.

---
+++ day149.py
@@ -0,0 +1,8 @@
+# Create a child class Bus that will inherit all of the variables and methods of the Vehicle class
+
+class Vehicle:
+
+    def __init__(self, name, max_speed, mileage):
+        self.name = name
+        self.max_speed = max_speed
+        self.mileage = mileage
