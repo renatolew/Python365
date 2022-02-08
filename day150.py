e0598669e91b5f89d54a86afeff9fc220363ce2c
Renato Lewkowicz <renato.lew@gmail.com>
2022-02-08 19:29:47 -0300

    Day 150 exercise.

---
+++ day150.py
@@ -0,0 +1,17 @@
+# Create a Bus class that inherits from the Vehicle class. Give the capacity argument of Bus.seating_capacity() a default value of 50.
+
+class Vehicle:
+    def __init__(self, name, max_speed, mileage):
+        self.name = name
+        self.max_speed = max_speed
+        self.mileage = mileage
+
+    def seating_capacity(self, capacity):
+        return f"The seating capacity of a {self.name} is {capacity} passengers"
+
+class Bus(Vehicle):
+    def seating_capacity(self, capacity=50):
+        return super().seating_capacity(capacity=50)
+
+School_bus = Bus("School Volvo", 180, 12)
+print(School_bus.seating_capacity())
