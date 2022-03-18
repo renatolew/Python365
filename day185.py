8e9f0b6ec56ca75324cd47e47261404d827456a5
Renato Lewkowicz <renato.lew@gmail.com>
2022-03-17 22:09:19 -0300

    Day 185 exercise.

---
+++ day185.py
@@ -0,0 +1,11 @@
+# Create a Python set such that it shows the element from both lists in a pair
+
+first_list = [2, 3, 4, 5, 6, 7, 8]
+print("First List: ", first_list)
+
+second_list = [4, 9, 16, 25, 36, 49, 64]
+print("Second List: ", second_list)
+
+result = zip(first_list, second_list)
+result_set = set(result)
+print(result_set)
