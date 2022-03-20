ead53c3a28af3c4d8e6c8bea46d2b3be4ec791f2
Renato Lewkowicz <renato.lew@gmail.com>
2022-03-20 01:00:06 -0300

    Day 186 exercise.

---
+++ day186.py
@@ -0,0 +1,14 @@
+# Find the intersection (common) of two sets and remove those elements from the first set
+
+first_set = {23, 42, 65, 57, 78, 83, 29}
+second_set = {57, 83, 29, 67, 73, 43, 48}
+
+print("First Set ", first_set)
+print("Second Set ", second_set)
+
+intersection = first_set.intersection(second_set)
+print("Intersection is ", intersection)
+for item in intersection:
+    first_set.remove(item)
+
+print("First Set after removing common element ", first_set)
