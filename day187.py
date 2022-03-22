059458895862c1e7ba0f7c776cf24e0a38d1071e
Renato Lewkowicz <renato.lew@gmail.com>
2022-03-21 22:12:37 -0300

    Day 187 exercise.

---
+++ day187.py
@@ -0,0 +1,22 @@
+# Checks if one set is a subset or superset of another set. If found as subset, delete all elements from that set:
+
+first_set = {57, 83, 29}
+second_set = {57, 83, 29, 67, 73, 43, 48}
+
+print("First Set ", first_set)
+print("Second Set ", second_set)
+
+print("First set is subset of second set -", first_set.issubset(second_set))
+print("Second set is subset of First set - ", second_set.issubset(first_set))
+
+print("First set is Super set of second set - ", first_set.issuperset(second_set))
+print("Second set is Super set of First set - ", second_set.issuperset(first_set))
+
+if first_set.issubset(second_set):
+    first_set.clear()
+
+if second_set.issubset(first_set):
+    second_set.clear()
+
+print("First Set ", first_set)
+print("Second Set ", second_set)
