a78ad6d73cbba0461e523702bbadcd40832ddc6e
Renato Lewkowicz <renato.lew@gmail.com>
2022-03-23 23:28:33 -0300

    Day 189 exercise.

---
+++ day189.py
@@ -0,0 +1,15 @@
+# Get all values from the dictionary and add them to a list but don’t add duplicates
+
+speed = {'jan': 47, 'feb': 52, 'march': 47, 'April': 44, 'May': 52, 'June': 53,
+         'july': 54, 'Aug': 44, 'Sept': 54}
+
+print("Dictionary's values - ", speed.values())
+
+speed_list = list()
+
+# iterate dict values
+for val in speed.values():
+    # check if value not present in a list
+    if val not in speed_list:
+        speed_list.append(val)
+print("unique list", speed_list)
