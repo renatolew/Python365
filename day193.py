4ea20e2162c4de36358839b1ce11e5b9d1ac05c2
Renato Lewkowicz <renato.lew@gmail.com>
2022-03-27 11:59:05 -0300

    Day 193 exercise.

---
+++ day193.py
@@ -0,0 +1,6 @@
+# Write a Python program to return a new set with unique items from both sets by removing duplicates.
+
+set1 = {10, 20, 30, 40, 50}
+set2 = {30, 40, 50, 60, 70}
+
+print(set1.union(set2))
