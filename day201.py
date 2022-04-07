646b640f2b8e99a8e8ec9f1c2083fcb453f2e2f5
Renato Lewkowicz <renato.lew@gmail.com>
2022-04-07 14:16:25 -0300

    Day 201 exercise.

---
+++ day201.py
@@ -0,0 +1,5 @@
+# Given is a nested tuple. Write a program to modify the first item (22) of a list inside a following tuple to 222
+
+tuple1 = (11, [22, 33], 44, 55)
+tuple1[1][0] = 222
+print(tuple1)
