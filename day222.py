cfea45208b0beecae16aad5e9edd28e709b5e758
renatolew <renato.lew@gmail.com>
2022-05-01 21:32:07 -0300

    Day 222 exercise.

---
+++ day222.py
@@ -0,0 +1,16 @@
+# Concatenate two data frames using the following conditions
+
+# GermanCars = {'Company': ['Ford', 'Mercedes', 'BMV', 'Audi'], 'Price': [23845, 171995, 135925 , 71400]}
+# japaneseCars = {'Company': ['Toyota', 'Honda', 'Nissan', 'Mitsubishi '], 'Price': [29995, 23600, 61500 , 58900]}
+
+import pandas as pd
+
+GermanCars = {'Company': ['Ford', 'Mercedes', 'BMV', 'Audi'], 'Price': [23845, 171995, 135925 , 71400]}
+german_df = pd.DataFrame.from_dict(GermanCars)
+
+japaneseCars = {'Company': ['Toyota', 'Honda', 'Nissan', 'Mitsubishi '], 'Price': [29995, 23600, 61500 , 58900]}
+japanese_df = pd.DataFrame.from_dict(japaneseCars)
+
+cars_df = pd.concat([german_df, japanese_df], keys=["Germany", "Japan"])
+
+print(cars_df)
