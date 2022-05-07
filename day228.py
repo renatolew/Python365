206eb53a88f347a4dcc018013ebf111edf0dd130
renatolew <renato.lew@gmail.com>
2022-05-07 17:47:31 -0300

    Day 228 exercise.

---
+++ day228.py
@@ -0,0 +1,7 @@
+# Accept any three string from one input() call
+
+string1, string2, string3 = input("Enter three strings:").split()
+
+print('First name:', string1)
+print('Second name:', string2)
+print('Third name:', string3)
