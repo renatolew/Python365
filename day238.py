b03d40d1abdc375f3671e6d39da783494f29c093
renatolew <renato.lew@gmail.com>
2022-05-19 09:48:02 -0300

    Day 238 exercise.

---
+++ day238.py
@@ -0,0 +1,6 @@
+# Write a program to find how many times substring “Emma” appears in the given string.
+
+str_sample = "Emma is good developer. Emma is a writer. Emma also loves coffee."
+
+emma_count = str_sample.count("Emma")
+print(emma_count)
