3eadf8d49555def354498c634dcac5aa944d021a
renatolew <renato.lew@gmail.com>
2022-06-03 14:43:51 -0300

    Day 246 exercise.

---
+++ day246.py
@@ -0,0 +1,7 @@
+# Write a Python program to convert the temperature in degree centigrade to Fahrenheit
+
+c = input(" Enter temperature in Centigrade: ")
+
+f = (9*(int(c))/5)+32
+
+print(" Temperature in Fahrenheit is: ", f)
