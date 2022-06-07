cd72b01e3d83076eced4d2e6b4aac2fa0102a615
renatolew <renato.lew@gmail.com>
2022-06-07 14:35:01 -0300

    Day 249 exercise.

---
+++ day249.py
@@ -0,0 +1,9 @@
+# Write a Python program to find the product of a set of real numbers
+
+i = 0
+product = 1
+count = int(input("Enter the number of real numbers: "))
+for i in range(count):
+    x = float(input("Enter a real number: "))
+    product = product * x
+print("The product of the numbers is: ", product)
