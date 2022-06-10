9250a3537f76fb923e25dce75cb48528b733424f
renatolew <renato.lew@gmail.com>
2022-06-10 09:36:07 -0300

    Day 252 exercise.

---
+++ day253.py
@@ -0,0 +1,13 @@
+# Python program to find the geometric mean of n numbers
+
+c = 0
+p = 1.0
+
+count = int(input("Enter the number of values: "))
+while(c < count):
+    x = float(input("Enter a real number: "))
+    c = c + 1
+    p = p * x
+
+gm = pow(p,1.0/count)
+print("The geometric mean is: ", gm)
