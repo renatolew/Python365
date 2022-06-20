4773f5f074995f4e28cfcf465c45e76f2847c007
renatolew <renato.lew@gmail.com>
2022-06-20 15:50:15 -0300

    Day 263 exercise.

---
+++ day263.py
@@ -0,0 +1,14 @@
+# Python program to implement linear search
+
+numbers = [4,2,7,1,8,3,6]
+f = 0
+
+x = int(input("Enter the number to be found out: "))
+
+for i in range(len(numbers)):
+    if (x == numbers[i]):
+        print("Successful search! The element is found at position", i)
+        f = 1
+        break
+if(f == 0):
+    print("Search unsuccessful...")
