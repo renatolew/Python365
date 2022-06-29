f2193e6c0c7bb7532437a8d25bafe09e9f40e130
renatolew <renato.lew@gmail.com>
2022-06-28 23:48:17 -0300

    Day 271 exercise.

---
+++ day271.py
@@ -0,0 +1,13 @@
+# Python program to check leap year
+
+year = int(input("Enter a year: "))
+if (year % 4) == 0:
+    if (year % 100) == 0:
+        if (year % 400) == 0:
+            print(year, " is a leap year")
+        else:
+            print(year, " is not a leap year")
+    else:
+        print(year, " is a leap year")
+else:
+    print(year, " is not a leap year")
