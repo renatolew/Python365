b4edf6afa66f8d0b7e576e2c910417b049d53367
renatolew <renato.lew@gmail.com>
2022-07-08 16:16:01 -0300

    Day 280 exercise.

---
+++ day280.py
@@ -0,0 +1,11 @@
+# Sort the list below in reverse alphabetic order and then assign the last element to the answer_1 variable.
+
+NFL_teams = ["Panthers", "Bears", "Dolphins" "Patriots", "Saints", "Giants"]
+
+NFL_teams.sort(reverse = True)
+
+print(NFL_teams)
+
+solution = NFL_teams[-1]
+
+print(solution)
