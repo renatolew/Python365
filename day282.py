197b4486caf24965b385435b324f77cda99e76a6
renatolew <renato.lew@gmail.com>
2022-07-10 14:35:02 -0300

    Day 282 exercise.

---
+++ day282.py
@@ -0,0 +1,10 @@
+# Can you slice the list so that it's reversed without using the .reverse() method?
+
+lst = [0,1,2,3,4]
+wrd = "Astrobingobaldolaringomarola"
+
+ans_1 = lst[::-1]
+ans_2 = wrd[::-1]
+
+print(ans_1)
+print(ans_2)
