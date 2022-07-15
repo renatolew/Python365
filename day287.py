f5ca257f726c66bb164ccd583d6bcf784c4e177d
renatolew <renato.lew@gmail.com>
2022-07-15 18:57:48 -0300

    Day 287 exercise.

---
+++ day287.py
@@ -0,0 +1,7 @@
+# Create a list from the elements of a range from 1200 to 2000 with steps of 130, using list comprehension.
+
+range_1 = range(1200, 2000, 130)
+
+list_1 = [i for i in range_1]
+
+print(list_1)
