ec3f6935a999fdb00ccd1ace7d31a32bed90aacf
renatolew <renato.lew@gmail.com>
2022-08-03 01:22:09 -0300

    Day 301 exercise.

---
+++ day301.py
@@ -0,0 +1,8 @@
+# Write a function which takes two arguments: a and b and returns the multiplication of them: a*b. Assign it to a variable named: f.
+
+
+i, j = 5, 10
+
+f = lambda a, b: a*b
+
+print(f(i, j))
