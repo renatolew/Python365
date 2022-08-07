deb53cf12e78ac9bfd813199139a3cecebe468ec
renatolew <renato.lew@gmail.com>
2022-08-07 14:10:32 -0300

    Day 305 exercise.

---
+++ day305.py
@@ -0,0 +1,6 @@
+# Using sorted() function and lambda sort the tuples in the list based on the second items.
+
+list1 = [(19542209, "New York") ,(4887871, "Alabama"), (1420491, "Hawaii"), (626299, "Vermont"), (1805832, "West Virginia"), (39865590, "California")]
+list1 = sorted(list1, key=lambda x: x[1])
+
+print(list1)
