fcba75d559bc7aaf15cc295f9c7f7fe5d0fcb0bb
renatolew <renato.lew@gmail.com>
2022-08-10 14:13:41 -0300

    Day 308 exercise.

---
+++ day308.py
@@ -0,0 +1,10 @@
+# Place a break statement in the for loop so that it prints from 0 to 7 only (including 7).
+
+# for i in range(100):
+#    print(i)
+
+
+for i in range(100):
+    print(i)
+    if i == 7:
+        break
