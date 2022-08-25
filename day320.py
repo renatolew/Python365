46823dd5b0f5479ba4c43f6464de1017890a032c
renatolew <renato.lew@gmail.com>
2022-08-24 23:11:21 -0300

    Day 320 exercise.

---
+++ day320.py
@@ -0,0 +1,13 @@
+# This time write a regex to get only the part of the email before the "@" sign excluding the "@" sign.
+# i.e: only mike part from mike@protonmail.com
+
+import re
+
+str = 'The advancements in biomarine studies franky@google.com, with the investments necessary and Davos sinatra123@yahoo.com Then The New Yorker article on wind farms...'
+
+
+regex = r'(\S+)@'
+emails = re.findall(regex, str)
+
+
+print(emails)
