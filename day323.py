c5683c1bb75332b00312bd51a7ae2b5c8ac90ae1
renatolew <renato.lew@gmail.com>
2022-08-27 00:58:02 -0300

    Day 323 exercise.

---
+++ day323.py
@@ -0,0 +1,14 @@
+# You are given stock prices for related financial tickers. (Symbols representing companies in the stock market)
+#
+# Find a way to extract the tickers mentioned in the report.
+# i.e.: TSLA, NFLX ...
+
+import re
+
+str = """Some of the prices were as following TSLA:749.50, ORCL: 50.50, GE: 10.90, MSFT: 170.50, BIDU: 121.40. As the macroeconomic developments continue we will update the prices. """
+
+regex = r'[A-Z]{2,}'
+data = re.findall(regex, str)
+
+
+print(data)
